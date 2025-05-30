f31300fb78b8908b2251d5c2420e631747d0235e
chtrn302 <traconghau0212@gmail.com>
2025-05-30 18:50:31 +0700

    Them Service xu ly Tra loi

--- chiasetailieuhoctapptit/src/main/java/com/chiasetailieu/chiasetailieuhoctapptit/service/BinhLuan/BinhLuanService.java
+++ chiasetailieuhoctapptit/src/main/java/com/chiasetailieu/chiasetailieuhoctapptit/service/BinhLuan/BinhLuanService.java
@@ -1,6 +1,7 @@
 package com.chiasetailieu.chiasetailieuhoctapptit.service.BinhLuan;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,8 @@ public class BinhLuanService {
 
     public List<BinhLuan> getCommentByDocumentId(long id){
         return binhLuanRepo.findByTaiLieu(id);
+    }
+    public Optional<BinhLuan> getBinhLuanById(int id) {
+        return binhLuanRepo.findById(id);
     }
 }
