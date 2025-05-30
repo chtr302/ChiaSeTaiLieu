fb935b08f4e075b80a923df23f96cbcc1afd37ce
chtrn302 <traconghau0212@gmail.com>
2025-05-31 00:19:00 +0700

    Thay doi ten bang khi ket voi Database

--- chiasetailieuhoctapptit/src/main/java/com/chiasetailieu/chiasetailieuhoctapptit/model/MonHocModel/FollowCourse.java
+++ chiasetailieuhoctapptit/src/main/java/com/chiasetailieu/chiasetailieuhoctapptit/model/MonHocModel/FollowCourse.java
@@ -2,10 +2,16 @@ package com.chiasetailieu.chiasetailieuhoctapptit.model.MonHocModel;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "FollowCourse", uniqueConstraints = @UniqueConstraint(columnNames = {"MaSinhVien", "MaMon"}))
+@Table(name = "TheoDoiMonHoc", uniqueConstraints = @UniqueConstraint(columnNames = {"MaSinhVien", "MaMon"}))
 public class FollowCourse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
