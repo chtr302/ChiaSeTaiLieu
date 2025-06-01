f71914d5beca175fb4e63339d53a3bb4eb6a9f43
chtrn302 <traconghau0212@gmail.com>
2025-06-01 21:13:40 +0700

    Them ham lay So luong Binh luan

--- chiasetailieuhoctapptit/src/main/java/com/chiasetailieu/chiasetailieuhoctapptit/repository/BinhLuanRepository/BinhLuanRepo.java
+++ chiasetailieuhoctapptit/src/main/java/com/chiasetailieu/chiasetailieuhoctapptit/repository/BinhLuanRepository/BinhLuanRepo.java
@@ -3,6 +3,8 @@ package com.chiasetailieu.chiasetailieuhoctapptit.repository.BinhLuanRepository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.chiasetailieu.chiasetailieuhoctapptit.model.BinhLuanModel.BinhLuan;
@@ -10,4 +12,9 @@ import com.chiasetailieu.chiasetailieuhoctapptit.model.BinhLuanModel.BinhLuan;
 @Repository
 public interface BinhLuanRepo extends JpaRepository<BinhLuan, Integer> {
     public List<BinhLuan> findByTaiLieu(long id);
+
+    @Query("SELECT " +
+           "(SELECT COUNT(bl) FROM BinhLuan bl WHERE bl.sinhVien = :maSinhVien) + " +
+           "(SELECT COUNT(tr) FROM TraLoiBinhLuan tr WHERE tr.maSinhVien = :maSinhVien)")
+    long countCommentsOfSinhVien(@Param("maSinhVien") String maSV);
 }
